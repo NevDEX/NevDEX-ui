806b6be15b59a5958759b2caaaf32393d3e3814a
fsy412 <fsy412@gmail.com>
2022-08-06 11:41:24 +0800

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const BaseUrl = "http://testnet.nevdex.tech:5000/"
-const BaseUrl = 'http://127.0.0.1:5000/api/'
+const BaseUrl = "https://testnet.nevdex.tech/api/"
+// const BaseUrl = 'http://127.0.0.1:5000/api/'
 
 // create an axios instance
 const service = axios.create({
