e9eef77d5414f2e7ab2480705d3f83042e27b67a
nevdex <fsy412@gmail.com>
2023-04-10 23:44:12 +0800

    Update NFT.vue

--- src/views/NFT.vue
+++ src/views/NFT.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container max-h-full   flex flex-row mx-auto mt-36 my-40">
-    <div class="absolute inset-x-0 md:top-10 min-h-0 pl-20 py-24 flex overflow-hidden z-0"><span
+  <div class="container max-h-full flex flex-row mx-auto mt-20  ">
+    <div class="absolute inset-x-0 md:top-10 min-h-0 pl-20 flex overflow-hidden z-0 py-60 pt-20 my-auato"><span
         class="block bg-[#ef233c] w-72 h-72 rounded-full mix-blend-multiply filter blur-3xl opacity-10 lg:w-96 lg:h-96"></span><span
         class="block bg-[#04868b] w-72 h-72 -ml-20 mt-40 rounded-full mix-blend-multiply filter blur-3xl opacity-10 lg:w-96 lg:h-96 nc-animation-delay-2000"></span>
     </div>
     <div class="text-white h-full items-center justify-center flex flex-col my-auto ">
-      <div class="text-3xl font-semibold md:text-4xl xl:text-5xl text-neutral-100  flex flex-col space-y-2">
+      <div class="text-3xl font-semibold md:text-4xl xl:text-5xl text-neutral-100 pt-10 flex flex-col space-y-2">
         <span> The leading</span>
         <span> NFT Marketplace</span>
         <span> on Nervos Godwoken</span>
@@ -15,7 +15,7 @@
         life with $NDX</p>
     </div>
 
-    <div class="mx-auto"><img class="w-full" src="../assets/about-hero-right.png" alt="" /></div>
+    <div class="mx-auto pt-10"><img class="w-full" src="../assets/about-hero-right.png" alt="" /></div>
   </div>
   <CreateAndSell />
   <MarketPlace />
