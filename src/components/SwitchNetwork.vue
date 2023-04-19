8454cf42ede29d842f1912ba33de8e9677d5fba8
NevDEX <fsy412@gmail.com>
2023-04-19 14:31:04 +0800

    Update SwitchNetwork.vue

--- src/components/SwitchNetwork.vue
+++ src/components/SwitchNetwork.vue
@@ -1,43 +1,38 @@
 <template>
-    <div v-if="show" class="flex justify-end items-center text-white cursor-pointer mr-3 font-bold" @click="onSwithNetwork">
-        Swith Network
-    </div>
+  <div v-if="show" class="flex justify-end items-center text-white cursor-pointer mr-3 font-bold" @click="onSwitchNetwork">Swith Network</div>
 
-    <SwitchNetwork :show="showModal" @close="showModal = false" />
+  <SwitchNetwork :show="showModal" @close="showModal = false" />
 </template>
 <script>
-import { mapGetters } from 'vuex'
 import SwitchNetwork from './modal/SwitchNetwork.vue'
 
 export default {
-    name: 'SwithNetwork',
-    data() {
-        return {
-            show: false,
-            showModal: false,
-        }
+  name: 'SwithNetwork',
+  data() {
+    return {
+      show: false,
+      showModal: false,
+    }
+  },
+  components: {
+    SwitchNetwork,
+  },
+  computed: {},
+  created() {},
+  watch: {
+    $route(to, from) {
+      this.show = true
+      if (to.fullPath === '/') {
+        this.show = false
+      }
     },
-    components: {
-        SwitchNetwork
+  },
+  methods: {
+    onSwitchNetwork() {
+      console.log('onSwitchNetwork')
+      this.showModal = true
     },
-    computed: {
-
-    },
-    created() { },
-    watch: {
-        $route(to, from) {
-            this.show = true
-            if (to.fullPath === '/') {
-                this.show = false
-            }
-        },
-    },
-    methods: {
-        onSwithNetwork() {
-            console.log('onSwithNetwork')
-            this.showModal = true
-        }
-    },
+  },
 }
 </script>
 <style scoped></style>
